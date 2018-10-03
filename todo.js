afe483b589ad3df658ab6b2b88cd86e1368ad6f2
bhavneshmittal <43740189+bhavneshmittal@users.noreply.github.com>
2018-10-03 21:37:30 +0530

    Delete todo.js

--- todo.js
+++
@@ -1,7 +0,0 @@
-function todolist() {
-	var item=document.getELementById("todoinput").value
-	var text=document.createTextNode(item)
-	var newitem=document.createELement("li")
-	newitem.appendChild(text)
-	document.getElementById("todolist").appendChild(newitem)
-}
